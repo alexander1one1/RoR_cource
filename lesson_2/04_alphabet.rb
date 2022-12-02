9eca13fdf8db4fcc94bb188f39ffd30a74cf4bf3
alexander1one1 <lepimolomu@doanart.com>
2022-12-03 01:28:22 +0300

    Fix 02 04

--- lesson_2/04_alphabet.rb
+++ lesson_2/04_alphabet.rb
@@ -1,4 +1,2 @@
 #!/usr/bin/ruby
-letters = Hash.new
-('a'..'z').each_with_index {|l,i| letters[l] = i + 1}
-puts letters
+puts ('a'..'z').each_with_index {|l,i| letters[l] = i + 1 if 'aeiouy'.include? l}
