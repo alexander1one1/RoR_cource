f03844834f676e8390a6e949f1774f2c4deb9556
alexander1one1 <lepimolomu@doanart.com>
2022-12-19 23:40:53 +0300

    First 2nd oop

---
+++ oop/main.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+# frozen_string_literal: true
+require_relative 'station'
+require_relative 'route'
+require_relative 'train'
+
+
+
---
+++ oop/train_cargo.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class TrainCargo < Train
+
+end
